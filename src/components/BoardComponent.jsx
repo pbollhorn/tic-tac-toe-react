3cd98f317dde600fae92908d9720891273a70db0
Peter Bollhorn <pbollhorn@gmail.com>
2025-04-29 10:38:36 +0200

    swithced to 1D array

--- src/components/BoardComponent.jsx
+++ src/components/BoardComponent.jsx
@@ -2,12 +2,15 @@ import React, { useState } from "react";
 import CellComponent from "./CellComponent";
 
 function BoardComponent() {
+  // The board is represented by a 1D array, whose indexes represents the 2D board like this:
+  // 0 | 1 | 2
+  // ----------
+  // 3 | 4 | 5
+  // ----------
+  // 6 | 7 | 8
+
   // State to store the board (0=blank, 1=cross, 2=nought))
-  const [board, setBoard] = useState([
-    [0, 0, 0],
-    [0, 0, 0],
-    [0, 0, 0],
-  ]);
+  const [board, setBoard] = useState([0, 0, 0, 0, 0, 0, 0, 0, 0]);
 
   // State to store player (1=cross, 2=nought)
   const [player, setPlayer] = useState(1);
@@ -16,15 +19,13 @@ function BoardComponent() {
     const id = event.target.id;
     if (id.substring(0, 4) !== "cell") return;
 
-    const row = parseInt(id.substring(4, 5));
-    const col = parseInt(id.substring(5, 6));
+    const cellNumber = parseInt(id.substring(4, 5));
+    if (board[cellNumber] !== 0) return;
 
-    if (board[row][col] !== 0) return;
-
-    const boardDeepCopy = board.map((row) => [...row]);
-    boardDeepCopy[row][col] = player;
+    const boardDeepCopy = [...board];
+    boardDeepCopy[cellNumber] = player;
     setBoard(boardDeepCopy);
-    
+
     setPlayer((player % 2) + 1);
   }
 
@@ -34,35 +35,35 @@ function BoardComponent() {
       <tbody>
         <tr>
           <td>
-            <CellComponent id="cell00" value={board[0][0]} />
+            <CellComponent id="cell0" value={board[0]} />
           </td>
           <td>
-            <CellComponent id="cell01" value={board[0][1]} />
+            <CellComponent id="cell1" value={board[1]} />
           </td>
           <td>
-            <CellComponent id="cell02" value={board[0][2]} />
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <CellComponent id="cell10" value={board[1][0]} />
-          </td>
-          <td>
-            <CellComponent id="cell11" value={board[1][1]} />
-          </td>
-          <td>
-            <CellComponent id="cell12" value={board[1][2]} />
+            <CellComponent id="cell2" value={board[2]} />
           </td>
         </tr>
         <tr>
           <td>
-            <CellComponent id="cell20" value={board[2][0]} />
+            <CellComponent id="cell3" value={board[3]} />
           </td>
           <td>
-            <CellComponent id="cell21" value={board[2][1]} />
+            <CellComponent id="cell4" value={board[4]} />
           </td>
           <td>
-            <CellComponent id="cell22" value={board[2][2]} />
+            <CellComponent id="cell5" value={board[5]} />
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <CellComponent id="cell6" value={board[6]} />
+          </td>
+          <td>
+            <CellComponent id="cell7" value={board[7]} />
+          </td>
+          <td>
+            <CellComponent id="cell8" value={board[8]} />
           </td>
         </tr>
       </tbody>
